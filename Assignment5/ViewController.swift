8ab896134c8ce1a80e6a70c45c95154724aa38b2
Justin Gray <jugray@gmail.com>
2023-11-24 16:14:04 -0600

    Added switch and button actions

--- Assignment5/ViewController.swift
+++ Assignment5/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usdField: UITextField!
+    
+    var enableEuro = true
+    var enableYen = true
+    var enablePound = true
+    var enableFranc = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func euroSwitch(_ sender: Any) {
+    }
+    
+    @IBAction func yenSwitch(_ sender: Any) {
+    }
+    
+    @IBAction func poundSwitch(_ sender: Any) {
+    }
+    
+    @IBAction func francSwitch(_ sender: Any) {
+    }
+    
+    @IBAction func convertButton(_ sender: Any) {
+    }
 }
 
